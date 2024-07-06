a612d73414a069dbc1474ad668cb8f5d9a67e6b4
han2003dz <han20031605@gmail.com>
2024-07-06 23:46:22 +0700

    add: basic project setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, TailwindCSS!</h1>
-    </div>
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">
+          Hello, TailwindCSS!
+        </h1>
+      </div>
+    </>
   );
 }
 
