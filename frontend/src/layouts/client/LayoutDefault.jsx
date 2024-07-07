8d8e32d8fbbcf5657daae2af3908c4e5a29ff8e0
han2003dz <han20031605@gmail.com>
2024-07-07 22:26:10 +0700

    update: LayoutDefault

--- frontend/src/layouts/client/LayoutDefault.jsx
+++ frontend/src/layouts/client/LayoutDefault.jsx
@@ -1,5 +1,31 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const navLinks = [
+  { to: "/", label: "Home" },
+  { to: "/register", label: "Register" },
+  { to: "/login", label: "Login" },
+  { to: "/users", label: "User" },
+];
+
 const LayoutDefault = () => {
-  return <div>Layout Default</div>;
+  return (
+    <>
+      <header>
+        <div className="layout__default--menu">
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.to}>
+                <NavLink to={link.to}>{link.label}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default LayoutDefault;
