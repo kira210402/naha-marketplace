42c64aedd0c5e83dde1a9dcf6d76c5da1aafc082
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-07-09 17:42:50 +0700

    fix: update homePage fe

--- frontend/src/components/client/storePage/productList/ProductList.jsx
+++ frontend/src/components/client/storePage/productList/ProductList.jsx
@@ -1,10 +1,10 @@
-import ProductItem from '../productItem/ProductItem';
+import ProductCard from '../../productCard/ProductCard';
 
 const ProductList = ({products}) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   )
