3b268aa2ea12a8e34132d5dccd346fd4e5d7e0c2
han2003dz <han20031605@gmail.com>
2024-07-14 07:52:02 +0700

    fĩx: update user profile

--- frontend/src/services/user/index.jsx
+++ frontend/src/services/user/index.jsx
@@ -1,6 +1,12 @@
-import { get } from '../../utils/request';
+import { get, patch } from '../../utils/request';
 
 export const getUser = async (id) => {
   const result = await get(`/users/${id}`);
+  return result;
+};
+
+export const updateUser = async (options) => {
+  const result = await patch(`/users`, options);
+  console.log(result);
   return result;
 };
