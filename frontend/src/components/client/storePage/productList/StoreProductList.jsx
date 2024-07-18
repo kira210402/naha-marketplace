bb34051718a3a165b68c0ad85978a0ea6cb95ee3
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-07-18 17:16:06 +0700

    feat: add pagination in page store detail

--- frontend/src/components/client/storePage/productList/StoreProductList.jsx
+++ frontend/src/components/client/storePage/productList/StoreProductList.jsx
@@ -1,22 +1,50 @@
+import { useState } from 'react';
 import ProductCard from '../../productCard/ProductCard';
+import { Pagination } from 'flowbite-react';
 
 const StoreProductList = ({ products }) => {
+  const productsPerPage = 20;
+  const productsCount = products.length;
+  const totalPage = Math.ceil(productsCount / productsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onPageChange = (page) => setCurrentPage(page);
+
+  const currentProducts = products.slice(
+    (currentPage - 1) * productsPerPage,
+    currentPage * productsPerPage
+  );
 
   return (
     <>
       <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-        {products.map(product => {
-          return (
-            <div key={product.id} className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-              <ProductCard product={product} />
-            </div>
-          )
-        })}
+        {currentProducts.length !== 0 ?
+          currentProducts.map(product => {
+            return (
+              <div key={product.id} className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                <ProductCard product={product} />
+              </div>
+            );
+          }) :
+          <div>This collection has 0 product</div>
+        }
       </div>
+
+      {/* pagination */}
+      {productsCount !== 0 ?
+        <div className="flex overflow-x-auto sm:justify-center">
+          <Pagination
+            layout="navigation"
+            currentPage={currentPage}
+            totalPages={totalPage}
+            onPageChange={onPageChange}
+            showIcons
+          />
+        </div>
+        : <div></div>
+      }
     </>
-
-
-  )
+  );
 }
 
 export default StoreProductList;
