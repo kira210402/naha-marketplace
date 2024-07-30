b70b1cf8442d3d07bc95dfe2e8c1f9cdca0acc74
han2003dz <han20031605@gmail.com>
2024-07-30 16:03:48 +0700

    feat: cancel order item by id

--- frontend/src/components/client/OrderManagementPage/DeleteRecord.jsx
+++ frontend/src/components/client/OrderManagementPage/DeleteRecord.jsx
@@ -1,20 +1,59 @@
 import { CloseCircleOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
+import Swal from 'sweetalert2';
+import { cancelOrderItem } from '../../../services/stores';
 
-const DeleteRecord = () => {
-  // const { handleReload } = props;
-  // console.log(handleReload);
+const DeleteRecord = ({ data, onReload }) => {
+  const handleDelete = () => {
+    Swal.fire({
+      title: 'Bạn có chắc chắn?',
+      text: 'Muốn hủy đơn hàng này!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await cancelOrderItem(data.id);
+          console.log(response);
+          if (response?.code === 200) {
+            Swal.fire({
+              title: 'Thông báo',
+              text: response?.message,
+              icon: 'success',
+            });
+            onReload();
+          } else {
+            Swal.fire({
+              title: 'Thông báo!',
+              text: response?.message,
+              icon: 'error',
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            title: 'Error!',
+            text: 'An error occurred while deleting the order item.',
+            icon: 'error',
+          });
+          console.error('Error deleting order item:', error);
+        }
+      }
+    });
+  };
+
   return (
-    <>
-      <Button
-        title='Xóa'
-        icon={<CloseCircleOutlined />}
-        danger
-        size='small'
-        type='text'
-        // onClick={handleDelete}
-      />
-    </>
+    <Button
+      title='Cancel'
+      icon={<CloseCircleOutlined />}
+      danger
+      size='small'
+      type='text'
+      onClick={handleDelete}
+    />
   );
 };
 
